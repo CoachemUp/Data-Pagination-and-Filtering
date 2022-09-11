0f8dfeea46da2767b5248334bfe8323bec025fac
JCobb <jeffcobb1206@gmail.com>
2022-09-11 01:23:48 -0400

    working on extra credit

--- js/script.js
+++ js/script.js
@@ -3,57 +3,83 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-/***
- * variable for setting the number of students on page at one time
+/**
+ * @param {string} list
+ * @param {number} page
  */
-const perPage = 9;
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 function showPage(list, page) {
-   // create two variables which will represent the index for the first and last student on the page
-   let startIndex = page * perPage - perPage;
-   let endIndex = page * perPage;
-   // select the element with a class of `student-list` and assign it to a variable
+   let startIndex = page * 9 - 9;
+   let endIndex = page * 9;
    let studentList = document.querySelector(".student-list");
-   // set the innerHTML property of the variable you just created to an empty string
    studentList.innerHTML = "";
-   // loop over the length of the `list` parameter
    for (let i = 0; i < list.length; i++) {
-      let studentItem = list[i];
-      // inside the loop create a conditional to display the proper student
-      if (i >= startIndex && i < endIndex) {
-         studentList +=
-            ` <li class="student-item cf">
-                  <div class="student-details">
-                     <img class="avatar" src="${studentItem['picture'].thumbnail}">
-                     <h3>
-                        ${studentItem['name'].title}
-                        ${studentItem['name'].first}
-                        ${studentItem['name'].last}
-                     </h3>
-                     <span class="email">${studentItem.email}</span>
-                  </div>
-                  <div class="joined-details">
-                     <span class="date">Joined
-                        ${studentItem['registered'].date}
-                     </span>
-                  </div>
-               </li>
-               `;
+      if (i >= startIndex && endIndex > i) {
+         let studentItem = ` <li class="student-item cf">
+                     <div class="student-details">
+                        <img class="avatar" src="${list[i].picture.thumbnail}" alt='Profile Picture'>
+                        <h3>
+                           ${list[i]["name"].first}
+                           ${list[i]["name"].last}
+                        </h3>
+                        <span class="email">${list[i].email}</span>
+                     </div>
+                     <div class="joined-details">
+                        <span class="date">Joined
+                           ${list[i]["registered"].date}
+                        </span>
+                     </div>
+                  </li>
+                  `;
+         studentList.insertAdjacentHTML("beforeend", studentItem);
       }
-
-
-      // insert the above elements
-      document.querySelector('.student-list').insertAdjacentHTML('beforeend', studentList);
    }
 }
-showPage(data, 1)
+
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+function addPagination(list) {
+   // create a variable to calculate the number of pages needed
+   let numOfPages = Math.ceil(list.length / 9);
+   // select the element with a class of `link-list` and assign it to a variable
+   let linkList = document.querySelector('.link-list');
+   // set the innerHTML property of the variable you just created to an empty string
+   linkList.innerHTML = "";
+   // loop over the number of pages needed
+   for (let i = 1; i <= numOfPages; i++) {
+      // create the elements needed to display the pagination button
+      let button = `
+            <li>
+               <button type="button">${i}</button>
+            </li>
+      `;
+      linkList.insertAdjacentHTML("beforeend", button); // insert the above elements
+   };
+   // give the first pagination button a class of "active"
+   document.querySelector('button').classList.add('active');
+
+   // create an event listener on the `link-list` element
+   linkList.addEventListener('click', function (e) {
+      //if click is on a button 
+      if (e.target.getAttribute('type') === 'button') {
+         //removes active class and sets it to empty string
+         document.querySelector('.active').classList.remove('active');
+         //adds active class to button that was clicked
+         e.target.classList.add('active');
+         //get number from button clicked
+         let text = e.target.textContent;
+         //display the correct page that user has clicked on 
+         showPage(data, text);
+      }  
+   }) 
+}
 
 // Call functions
+showPage(data, 1);
+addPagination(data)
